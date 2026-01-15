30824a06b7f7a0653f1a0785e37261e81e79a212
Robbert Brandsma <i@robbertbrandsma.nl>
2026-01-15 10:48:51 +0100

    Move Screenshot to Circle thumb key, add mouse click to Heart
    - Screenshot moved from Heart (row 3) to Circle (row 4, outermost)
    - Added MS_BTN1 (mouse click) to the Heart position for trackpad use

--- keyboards/splitkb/halcyon/kyria/keymaps/obbut/keymap.c
+++ keyboards/splitkb/halcyon/kyria/keymaps/obbut/keymap.c
@@ -24,8 +24,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DEFAULT] = LAYOUT_split_3x6_5_hlc(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_B ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_BSPC,
      KC_ESC  , KC_A ,  KC_R   ,  KC_S  ,   KC_T ,   KC_G ,                                        KC_M,   KC_N ,  KC_E ,   KC_I ,  KC_O , KC_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , KC_LOPT, SCREENSHOT,  FKEYS  , KC_NO  , KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_SLSH, KC_ENT ,
-                                KC_NO  , KC_LCTL, KC_LGUI, AEROSPACE, KC_SPC,     KC_NO  , KC_SPC , RAISE , LOWER  , KC_NO  ,
+     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , KC_LOPT, MS_BTN1 ,  FKEYS  , KC_NO  , KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_SLSH, KC_ENT ,
+                                SCREENSHOT, KC_LCTL, KC_LGUI, AEROSPACE, KC_SPC,     KC_NO  , KC_SPC , RAISE , LOWER  , KC_NO  ,
      KC_NO  , KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_NO  , KC_NO, KC_NO, KC_NO, KC_NO
     ),
 
