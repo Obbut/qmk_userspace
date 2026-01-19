188d7f8feafeafd6ae1fa679eb3100a67c4e4756
Robbert Brandsma <i@robbertbrandsma.nl>
2026-01-19 11:58:07 +0100

    Q15 Max: Move Aerospace to Escape hold, Option to third position
    - Escape key now sends Esc on tap, Aerospace (Ctrl+Cmd+Opt) on hold
    - Option key moved to the third position (former Aerospace location)
    - Second modifier position is now blank

--- keyboards/keychron/q15_max/ansi_encoder/keymaps/obbut/keymap.c
+++ keyboards/keychron/q15_max/ansi_encoder/keymaps/obbut/keymap.c
@@ -29,6 +29,9 @@ enum layers {
 // Aerospace window manager modifier (Cmd+Ctrl+Opt)
 #define AEROSPACE LCTL(LGUI(KC_RALT))
 
+// Escape on tap, Aerospace modifiers on hold
+#define ESC_AERO MT(MOD_LCTL | MOD_LGUI | MOD_RALT, KC_ESC)
+
 // macOS screenshot (Cmd+Ctrl+Shift+4)
 #define MAC_SCREENSHOT LGUI(LCTL(LSFT(KC_4)))
 
@@ -37,9 +40,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_66(
         MAC_SCREENSHOT,KC_1,    KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_BSPC,  KC_MPLY,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
-        KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
+        ESC_AERO, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_DEL,
-        KC_LCTL,  KC_LOPT,  AEROSPACE,KC_LCMD,       KC_SPC,             LT(_RAISE, KC_SPC),MO(MAC_FN),MO(COM_FN),KC_LEFT, KC_DOWN,  KC_RGHT),
+        KC_LCTL,  XXXXXXX,  KC_LOPT,  KC_LCMD,       KC_SPC,             LT(_RAISE, KC_SPC),MO(MAC_FN),MO(COM_FN),KC_LEFT, KC_DOWN,  KC_RGHT),
 
     [WIN_BASE] = LAYOUT_ansi_66(
         KC_PSCR,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_BSPC,  KC_MPLY,
